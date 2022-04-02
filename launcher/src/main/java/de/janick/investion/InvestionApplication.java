feb883dbf1049ac16421f44542667477ad1d7ee2
janickvdS <janick.van-der-sar@iubh-fernstudium.de>
2022-04-02 18:21:42 +0200

    Initial Commit

---
+++ launcher/src/main/java/de/janick/investion/InvestionApplication.java
@@ -0,0 +1,13 @@
+package de.janick.investion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestionApplication.class, args);
+    }
+
+}
---
+++ launcher/src/test/java/de/janick/investion/InvestionApplicationTests.java
@@ -0,0 +1,13 @@
+package de.janick.investion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
